a1bddca3a3587f64c985c46d9a0c9e78709d4fc9
bibhutighimire <ghimirebibhuti@gmail.com>
2020-08-20 22:23:15 -0600

    editabletimer and editable timerlist yut created; all static

--- src/components/EditableTimerList.js
+++ src/components/EditableTimerList.js
@@ -1,12 +1,16 @@
 import React from "react";
-
+import "../css/styles.css";
 import EditableTimer from "./EditableTimer";
 
 export default function EditableTimerList() {
   return (
-    <div id="timers">
+    <div className="editableTimerListContainer">
+<div className="editableTimerContainer">
       <EditableTimer />
+      </div>
+      <div className="editableTimerContainer">
       <EditableTimer />
+      </div>
     </div>
   );
 }
--- src/components/TimersDashboard.js
+++ src/components/TimersDashboard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../css/styles.css";
-import ReactDOM from "react-dom";
 
 import EditableTimerList from "./EditableTimerList";
 import ToggleableTimerForm from "./ToggleableTimerForm";
@@ -9,7 +8,7 @@ export default function TimersDashboard() {
   return (
     <div>
       <div className="container">
-        fefwef
+        
         <EditableTimerList />
         <ToggleableTimerForm />
       </div>
