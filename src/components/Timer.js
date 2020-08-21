301518521bf58a83206d363615810a3e6f5c9151
bibhutighimire <ghimirebibhuti@gmail.com>
2020-08-20 23:39:40 -0600

    added fontawesome delete and edit icons

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,12 +1,22 @@
 import React from "react";
-
+// import { faHeart, faHamburger } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTrash,
+  faEdit,
+} from "@fortawesome/free-solid-svg-icons";
 export default function Timer() {
   return (
     <div>
       <div className="timerContainer">
         <h1>Mow the lawn</h1>
         <p>House Chores</p>
-      <button className="timerBtn">Start</button>
+        <time>01:12:36</time>
+        <FontAwesomeIcon icon={faTrash} />
+        <FontAwesomeIcon icon={faEdit} />
+        
+
+      <button className="timerBtn">START</button>
       </div>
     </div>
   );
