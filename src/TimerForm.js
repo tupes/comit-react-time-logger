63455e771c56dc821e00c0b55e6898d70f3ed1f6
simrat1980 <simrat.k.sandhu@gmail.com>
2020-08-24 19:25:53 -0600

    initial commit

--- src/TimerForm.js
+++ src/TimerForm.js
@@ -1,21 +1,34 @@
 import React from "react";
 
-export default function TimerForm() {
+export default function TimerForm(props) {
+  const submitText= props.title?"Update":"Create";
+ const  handleSubmit = () => {
+    props.onFormSubmit({
+    id: 3,
+    title: "title",
+    project: "project"
+    });
+    };
+  
   return (
-    <div>
-      <div>
-        <div>
-          <div>
+    <div className="ui centered card">
+      <div className="column">
+        <div className="ui form">
+          <div className="field">
             <label>Title</label>
-            <input type="text" />
+            <input type="text" defaultValue={props.title} name="title"/>
           </div>
-          <div>
+          <div className="field">
             <label>Project</label>
-            <input type="text" />
+            <input type="text" defaultValue={props.project} name="project"/>
           </div>
-          <div>
-            <button></button>
-            <button></button>
+          <div className="ui two bottom attached buttons'>">
+            <button className="ui basic blue button" onClick={handleSubmit}>
+              {submitText}
+            </button>
+            <button className="ui basic red button" onClick={props.onFormClose}>
+              Cancel
+            </button>
           </div>
         </div>
       </div>
--- src/ToggleableTimerForm.js
+++ src/ToggleableTimerForm.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState} from "react";
 
 import TimerForm from "./TimerForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
-export default function ToggleableTimerForm() {
-  const isOpen = false;
+export default function ToggleableTimerForm(props) {
+  const [isOpen,setisOpen]=useState(false);
+  const handleFormOpen=()=>{
+    setisOpen(true);
+  }
+  const handleFormClose=()=>{
+    setisOpen(false);
+    
+  }
+  const handleFormSubmit=(timer)=>{
+    props.onFormSubmit(timer);
+    setisOpen(false);
+
+  }
+
 
   if (isOpen) {
-    return <TimerForm />;
+    return <TimerForm 
+    onFormSubmit={handleFormSubmit}
+    onFormClose={handleFormClose}
+    />;
   } else {
     return (
-      <div className="ui basic content center aligned segment">
-        <button className="ui basic button icon">
-          <i className="plus icon" />
+      <div className="toggleform">
+        <button onClick={handleFormOpen} className="plus button">
+        <i className="fa fa-plus-square">
+              <FontAwesomeIcon className="plusicon" icon={faPlus} />
+              </i>
         </button>
       </div>
     );
